435a4b380aa6a8e2c6fd86b1da28c1e00f0a4fdf
Erick Quispe <44407537+erickmomia12@users.noreply.github.com>
2018-10-29 14:25:06 +0100

    Update Jubilado,no jubilado.py

--- Jubilado,no jubilado.py
+++ Jubilado,no jubilado.py
@@ -5,7 +5,7 @@ precioFinal = 0;
 
 edad = int(input("Introduce tu edad: "))
 sexe = input("Introduce tu genero (M si eres mujer, H si eres hombre): ")
-colorCabell = int(input("Cual es el color de tu pelo? (1 Rubio/a, 2 Peliroja, 3 Morena, 0 para otro): "))
+colorCabell = int(input("Cual es el color de tu pelo? (1 Rubio/a, 2 Peliroja, 3 Morena, 0 otro): "))
 jubilat = input("Estas jubilado? (si, no): ")
 
 if sexe == "H":
