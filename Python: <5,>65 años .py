bf15eead29613cbe331d1998e22751543de9b503
Erick Quispe <44407537+erickmomia12@users.noreply.github.com>
2018-10-29 14:23:55 +0100

    Update Python: <5,>65 años .py

--- Python: <5,>65 años .py
+++ Python: <5,>65 años .py
@@ -4,12 +4,12 @@ precioFinal = 0;
 edad = int(input("Introduce tu edad: "))
 
 if edad <5:
-    precioFinal = 0;
+    preciofinal = 0;
 
 if edad >=65:
-    precioFinal = 0;
+    preciofinal = 0;
 
 if edad >5 and edad <65:
-    precioFinal = 2.15;
+    preciofinal = 2.15;
 
-print("El precio es:",precioFinal);
+print("El precio es:",preciofinal);
