0720aaff911b1b7cf842834cc28d06156ffe616b
Erick Quispe <44407537+erickmomia12@users.noreply.github.com>
2018-10-29 17:51:49 +0100

    Update Jubilado,no jubilado.py

--- Jubilado,no jubilado.py
+++ Jubilado,no jubilado.py
@@ -1,27 +1,27 @@
 edad = 0;
-sexe = 1;          #0 és mujer, 1 és hombre
-colorCabell = 0;
-precioFinal = 0;
+sexo = 1;          #0 és mujer, 1 és hombre
+colorCabello = 0;
+preciofinal = 0;
 
 edad = int(input("Introduce tu edad: "))
-sexe = input("Introduce tu genero (M si eres mujer, H si eres hombre): ")
-colorCabell = int(input("Cual es el color de tu pelo? (1 Rubio/a, 2 Peliroja, 3 Morena, 0 otro): "))
-jubilat = input("Estas jubilado? (si, no): ")
+sexo = input("Introduce tu genero (M si eres mujer, H si eres hombre): ")
+colorCabello = int(input("Cual es el color de tu pelo? (1 Rubio/a, 2 Peliroja, 3 Morena, 0 otro): "))
+jubilado = input("Estas jubilado? (si, no): ")
 
-if sexe == "H":
-    if jubilat == "no":
-        precioFinal == 1;
+if sexo == "H":
+    if jubilado == "no":
+        preciofinal == 1;
 
-if sexe == "M":
-    if jubilat == "no" and colorCabell == 1: 
-        precioFinal = 0;
+if sexo == "M":
+    if jubilado == "no" and colorCabello == 1: 
+        preciofinal = 0;
 
-if sexe == "H, M":
+if sexo == "H, M":
     if jubilat == "si" and edad > 65:
         precioFinal = 0;
 
-if sexe == "M":
-    if jubilat == "no" and colorCabell == 0:
-        precioFinal = 0.5;
+if sexo == "M":
+    if jubilado == "no" and colorCabello == 0:
+        preciofinal = 0.5;
 
-print("El precio final es:",precioFinal);
+print("El precio final es:",preciofinal);
