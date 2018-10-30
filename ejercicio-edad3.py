38d3d539b65b18d03d5868068e2c0ba419571c20
Erick Quispe <44407537+erickmomia12@users.noreply.github.com>
2018-10-30 12:37:21 +0100

    Update ejercicio-edad3.py

--- ejercicio-edad3.py
+++ ejercicio-edad3.py
@@ -1,4 +1,4 @@
 edad = int(input("Que edad tiene: "))
 
-if (edad >= 18) and (edad <= 23) or (edad == 17):
+if (edad >= 18 and edad <= 23) or (edad == 17):
     print ( "Puedes entrar en la sesión de jóvenes")
