bb7779878c3b01a02d5ddec51713945da82e5f51
Erick Quispe <44407537+erickmomia12@users.noreply.github.com>
2018-11-07 21:07:10 +0100

    Update Comparador de anyos.py

--- Comparador de anyos.py
+++ Comparador de anyos.py
@@ -1,10 +1,19 @@
 # coding:utf8
 anyo_actual = int(input("Que anyo estyamos? "))
-cualquier_anyo = int(input("Esciba cualquier anyo "))
+cualquier_anyo = int(input("Escriba un anyo cualquiera "))
 if (anyo_actual == 2018) and (cualquier_anyo >=2018):
     print ("Para llegar al anyo",cualquier_anyo, "faltan", cualquier_anyo - anyo_actual,"anyos")
     
- 
-
     
- 
+# coding:utf8
+anyo_actual = int(input("Que anyo estamos? "))
+cualquier_anyo = int(input("Escriba un anyo cualquiera "))
+if (anyo_actual ==2018) and (cualquier_anyo <= 2018):
+    print ("Desde el anyo",cualquier_anyo, "han pasado", anyo_actual - cualquier_anyo,"anyo")
+    
+    
+# coding:utf8
+anyo_actual = int(input("Que anyo estamos? "))
+cualquier_anyo = int(input("Escriba un anyo cualquiera "))
+if (anyo_actual ==2018) and (cualquier_anyo == 2018):
+    print ("Son el mismo anyo")
