068fad2def855dbafe35705d8e8434e56b1befb0
Erick Quispe <44407537+erickmomia12@users.noreply.github.com>
2018-11-07 21:00:16 +0100

    Create Comparador de anyos.py

---
+++ Comparador de anyos.py
@@ -0,0 +1,10 @@
+# coding:utf8
+anyo_actual = int(input("Que anyo estyamos? "))
+cualquier_anyo = int(input("Esciba cualquier anyo "))
+if (anyo_actual == 2018) and (cualquier_anyo >=2018):
+    print ("Para llegar al anyo",cualquier_anyo, "faltan", cualquier_anyo - anyo_actual,"anyos")
+    
+ 
+
+    
+ 
