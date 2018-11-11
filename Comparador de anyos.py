6d15f84716605a39e5fcae517f329b74ef36f810
Erick Quispe <44407537+erickmomia12@users.noreply.github.com>
2018-11-11 18:00:40 +0100

    Update Comparador de anyos.py

--- Comparador de anyos.py
+++ Comparador de anyos.py
@@ -1,19 +1,19 @@
 # coding:utf8
-anyo_actual = int(input("Que anyo estyamos? "))
-cualquier_anyo = int(input("Escriba un anyo cualquiera "))
-if (anyo_actual == 2018) and (cualquier_anyo >=2018):
-    print ("Para llegar al anyo",cualquier_anyo, "faltan", cualquier_anyo - anyo_actual,"anyos")
+anyo_actual = int(input("Que año estamos? "))
+cualquier_anyo = int(input("Escriba un año cualquiera "))
+if (anyo_actual >0) and (cualquier_anyo >0):
+    print ("Para llegar al año",cualquier_anyo, "faltan", cualquier_anyo - anyo_actual,"años")
     
     
 # coding:utf8
-anyo_actual = int(input("Que anyo estamos? "))
-cualquier_anyo = int(input("Escriba un anyo cualquiera "))
-if (anyo_actual ==2018) and (cualquier_anyo <= 2018):
-    print ("Desde el anyo",cualquier_anyo, "han pasado", anyo_actual - cualquier_anyo,"anyo")
+anyo_actual = int(input("Que año estamos? "))
+cualquier_anyo = int(input("Escriba un año cualquiera "))
+if (anyo_actual >0) and (cualquier_anyo >0):
+    print ("Desde el año",cualquier_anyo, "han pasado", anyo_actual - cualquier_anyo,"años")
     
     
 # coding:utf8
-anyo_actual = int(input("Que anyo estamos? "))
-cualquier_anyo = int(input("Escriba un anyo cualquiera "))
-if (anyo_actual ==2018) and (cualquier_anyo == 2018):
-    print ("Son el mismo anyo")
+anyo_actual = int(input("Que año estamos? "))
+cualquier_anyo = int(input("Escriba un año cualquiera "))
+if (anyo_actual >0) and (cualquier_anyo >0):
+    print ("Son el mismo año")
