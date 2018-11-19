6127f0d8a33ebc71323015460441b752dda9548a
Erick Quispe <44407537+erickmomia12@users.noreply.github.com>
2018-11-19 17:52:23 +0100

    Create ejercicios-pares.py

---
+++ ejercicios-pares.py
@@ -0,0 +1,4 @@
+alumnos = int(input("Cuantos alumnos tienes "))
+euro = int(input("Cuantos euros daras cada uno? "))
+print("Que bonito número", euro & alumnos, "par")
+print("Qué numero", alumnos // euro, "mas vulgar")
