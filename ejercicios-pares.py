2d7c156ad3e3c9528e047ed54df659f8d820be42
Erick Quispe <44407537+erickmomia12@users.noreply.github.com>
2018-11-20 10:11:15 +0100

    Update ejercicios-pares.py

--- ejercicios-pares.py
+++ ejercicios-pares.py
@@ -1,5 +1,5 @@
 alumnos = int(input("Cuantos alumnos tienes? "))
 euro = int(input("Cuantos euros daras cada uno? "))
-print("Aún quedan", euro % alumnos, "euro")
+print("Aún quedan", alumnos % euro, "euro")
 print("Qué numero", euro // alumnos, "mas vulgar")
 print("Qué numero", euro / alumnos, "mas vulgar")
