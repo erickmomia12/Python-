cd6b7dc3c169707d505cf086a5981da0822c40da
Erick Quispe <44407537+erickmomia12@users.noreply.github.com>
2018-11-29 15:02:07 +0100

    Delete ejercicios-pares.py

--- ejercicios-pares.py
+++
@@ -1,5 +0,0 @@
-numero = int(input("Introduce cualquier numero:"))   
-if (numero % 2 == 0):  
-    print("Es par, que bonito numero par")       
-else:   
-    print("Es impar, que numero mas vulgar") 
